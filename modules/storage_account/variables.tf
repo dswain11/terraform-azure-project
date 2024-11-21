55b7fffa6a87acc546482f7bb5a0dc7596b4c092
dswain11 <dreswain_11@yahoo.com>
2024-11-21 09:53:25 -0600

    Create variables.tf
    Add modules/storage_account/variables.tf

---
+++ modules/storage_account/variables.tf
@@ -0,0 +1,13 @@
+variable "storage_account_name" {
+ description = "Name of the Azure Storage Account"
+ type = string
+ default = "terraform-storage"
+}
+variable "resource_group_name" {
+ description = "Name of the Resource Group"
+ type = string
+}
+variable "location" {
+ description = "Azure location"
+ type = string
+}
